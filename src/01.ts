551d832e5955b4c10263f3ea4f60f54275fafa48
RayWiis <raywiis@live.com>
2021-12-01 22:55:02 +0200

    Minor renaming

--- src/01.ts
+++ src/01.ts
@@ -1,15 +1,16 @@
 const input = await Deno.readTextFile("src/01.input.txt");
-const depths = input.split("\n").map((num) => parseInt(num, 10));
+const data = input.split("\n").map((num) => parseInt(num, 10));
 
 const getPeaks = (depths: number[]) =>
 	depths
 		.map((num, idx, arr) => num > arr[idx - 1])
 		.reduce((acc, curr) => (curr ? acc + 1 : acc), 0);
 
-const singlePeaks = getPeaks(depths);
+const singlePeaks = getPeaks(data);
+
 console.log(`Single dips ${singlePeaks}`);
 
-const windowedDepths = depths
+const windowedDepths = data
 	.map((_, idx, arr) =>
 		idx < arr.length - 2 ? arr[idx] + arr[idx + 1] + arr[idx + 2] : undefined
 	)
