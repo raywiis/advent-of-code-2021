f1d424593d5c6f8463fe3bed051d767e314b6777
RayWiis <raywiis@live.com>
2021-12-04 19:29:53 +0200

    Run prettier

--- src/03/03.ts
+++ src/03/03.ts
@@ -4,25 +4,29 @@ import {
 	join,
 } from "https://deno.land/std@0.116.0/path/mod.ts";
 
-const inputFilePath = join(dirname(fromFileUrl(import.meta.url)), '03.input.txt');
+const inputFilePath = join(
+	dirname(fromFileUrl(import.meta.url)),
+	"03.input.txt"
+);
 const input = await Deno.readTextFile(inputFilePath);
 
-const data = input.split('\n').map(a => a.trim());
+const data = input.split("\n").map((a) => a.trim());
 
-const initialFrequencies = [...data[0]].map(_ => 0);
+const initialFrequencies = [...data[0]].map((_) => 0);
 const addFrequencies = (frequencies: number[], bits: string) =>
 	frequencies.map((f, idx) => (bits[idx] === "1" ? f + 1 : f));
 
 const oneFrequencies = data.reduce(addFrequencies, initialFrequencies);
-const zeroFrequencies = oneFrequencies.map(f => data.length - f);
+const zeroFrequencies = oneFrequencies.map((f) => data.length - f);
 
-const gammaBits = oneFrequencies
-	.map((f1, idx) => (f1 >= zeroFrequencies[idx] ? 1 : 0))
+const gammaBits = oneFrequencies.map((f1, idx) =>
+	f1 >= zeroFrequencies[idx] ? 1 : 0
+);
 
-const epsilonBits = gammaBits.map((g) => g === 1 ? 0 : 1)
+const epsilonBits = gammaBits.map((g) => (g === 1 ? 0 : 1));
 
-const gamma = parseInt(gammaBits.join(''), 2);
-const epsilon = parseInt(epsilonBits.join(''), 2);
+const gamma = parseInt(gammaBits.join(""), 2);
+const epsilon = parseInt(epsilonBits.join(""), 2);
 
 export const powerRate = gamma * epsilon;
 
@@ -31,19 +35,19 @@ const advancedFrequencies = (mostCommon = true) => {
 
 	for (const idx in initialFrequencies) {
 		const freqs = workingBits.reduce(addFrequencies, initialFrequencies);
-		const bit = freqs[idx] >=(workingBits.length - freqs[idx]) ? '1' : '0';
-		
+		const bit = freqs[idx] >= workingBits.length - freqs[idx] ? "1" : "0";
+
 		workingBits = workingBits.filter((seq) =>
 			mostCommon ? seq[idx] === bit : seq[idx] !== bit
 		);
 
 		if (workingBits.length === 1) {
-			return workingBits[0]
+			return workingBits[0];
 		}
 	}
 
-	throw new Error('goofed');
-}
+	throw new Error("goofed");
+};
 
 const oxygenRating = parseInt(advancedFrequencies(true), 2);
 const co2Rating = parseInt(advancedFrequencies(false), 2);
