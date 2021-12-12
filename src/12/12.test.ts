aff737b9451a5e70e0a4c9f9fc591c27a9dccb74
RayWiis <raywiis@live.com>
2021-12-12 12:59:12 +0200

    Refactor tests

--- src/12/12.test.ts
+++ src/12/12.test.ts
@@ -14,47 +14,27 @@ async function readInput(fileName: string): Promise<Cave> {
 	return data.map((row) => row.split("-")) as Cave;
 }
 
-const sample1 = await readInput("12.sample_1.txt");
-const sample2 = await readInput("12.sample_2.txt");
-const sample3 = await readInput("12.sample_3.txt");
-const input = await readInput("12.input.txt");
+const cases = (
+	[
+		["12.sample_1.txt", 10, 36],
+		["12.sample_2.txt", 19, 103],
+		["12.sample_3.txt", 226, 3509],
+		["12.input.txt", 4549, 120535],
+	] as [string, number, number][]
+).map(async ([fileName, part1Expectation, part2Expectation]) => {
+	const data = await readInput(fileName);
 
-Deno.test("part 1 sample 1", () => {
-	const paths = findPaths(sample1);
-	assertEquals(paths.length, 10);
+	const [day, sampleName] = fileName.split(".");
+
+	Deno.test(`day ${day} ${sampleName} part 1`, () => {
+		const paths = findPaths(data);
+		assertEquals(paths.length, part1Expectation);
+	});
+
+	Deno.test(`day ${day} ${sampleName} part 2`, () => {
+		const paths = findPathsMultivisit(data);
+		assertEquals(paths.size, part2Expectation);
+	});
 });
 
-Deno.test("part 1 sample 2", () => {
-	const paths = findPaths(sample2);
-	assertEquals(paths.length, 19);
-});
-
-Deno.test("part 1 sample 3", () => {
-	const paths = findPaths(sample3);
-	assertEquals(paths.length, 226);
-});
-
-Deno.test("part 1 input", () => {
-	const paths = findPaths(input);
-	assertEquals(paths.length, 4549);
-});
-
-Deno.test("part 2 sample 1", () => {
-	const paths = findPathsMultivisit(sample1);
-	assertEquals(paths.size, 36);
-});
-
-Deno.test("part 2 sample 2", () => {
-	const paths = findPathsMultivisit(sample2);
-	assertEquals(paths.size, 103);
-});
-
-Deno.test("part 2 sample 3", () => {
-	const paths = findPathsMultivisit(sample3);
-	assertEquals(paths.size, 3509);
-});
-
-Deno.test("part 2 input", () => {
-	const paths = findPathsMultivisit(input);
-	assertEquals(paths.size, 120535);
-});
+await Promise.all(cases);
