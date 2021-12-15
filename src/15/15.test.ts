ee78498050eb40fbed6b375e7d921fb26a1af837
RayWiis <raywiis@live.com>
2021-12-15 23:03:39 +0200

    run prettier

--- src/15/15.test.ts
+++ src/15/15.test.ts
@@ -13,31 +13,30 @@ async function readInput(fileName: string) {
 	const data = input.split(os.EOL());
 	const map = new Map();
 
-	data.forEach(
-		(row, rowIdx) => [...row].forEach((cell, colIdx) => {
+	data.forEach((row, rowIdx) =>
+		[...row].forEach((cell, colIdx) => {
 			map.set(toKey([rowIdx, colIdx]), parseInt(cell));
 		})
-	)
+	);
 
 	return map;
 }
-
 
 const sample1 = await readInput("15.sample.txt");
 const input = await readInput("15.input.txt");
 
 Deno.test("day 15 part 1 sample 1", () => {
 	assertEquals(findPathCost(sample1), 40);
-})
+});
 
 Deno.test("day 15 part 1 input", () => {
 	assertEquals(findPathCost(input), 462);
-})
+});
 
 Deno.test("day 15 part 2 sample 1", () => {
 	assertEquals(findPathCost(sample1, 5), 315);
-})
+});
 
 Deno.test("day 15 part 2 input", () => {
 	assertEquals(findPathCost(input, 5), 2846);
-})
+});
